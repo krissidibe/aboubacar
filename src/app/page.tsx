f333f7b0289b8924c2b53c704577460b75bb065f
krissidibe <assowlove@gmail.com>
2024-09-10 15:39:27 +0000

    First5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus provident libero rem architecto praesentium dolores? Amet, animi aliquam possimus, maiores rem libero, laudantium ducimus laboriosam cum illum vel beatae enim.</div>
+    <div>page lo
+
+      Aboubacar
+    </div>
   )
 }
 
