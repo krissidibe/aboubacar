966b4594fb60ed3b91ca286a8ec9c695a0e75131
krissidibe <assowlove@gmail.com>
2024-09-10 15:42:22 +0000

    First9

--- src/public/test.tsx
+++ src/public/test.tsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const Test = () => {
   return (
-    <div>Test xx
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad repellat deserunt facere laudantium fugit neque, distinctio iusto! Perferendis nemo maiores aliquam animi recusandae, ipsa veniam? Animi sunt vel aspernatur dolore?</p>
-    </div>
+    <div>Test</div>
   )
 }
 
