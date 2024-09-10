7e0ae85ed6433f9e138a9931987b2dc9ec72510f
krissidibe <assowlove@gmail.com>
2024-09-10 16:37:11 +0000

    Resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+}); 
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
