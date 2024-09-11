43aa3ccf33c82718a7116f969788dbd68fc1f728
krissidibe <assowlove@gmail.com>
2024-09-11 16:57:50 +0000

    Resume Post Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-
+import { Analytics } from "@vercel/analytics/react"
  
 
 const geistSans = localFont({
@@ -38,6 +38,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Analytics />
           </ThemeProvider>
 
       </body>
